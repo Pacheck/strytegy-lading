2a6bb294127d74f472da32dc1a486b6d4ad87d43
Pedro Pacheco <pacheckdev@gmail.com>
2021-08-06 19:31:42 -0300

    button style sheet

---
+++ src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CustomButton = styled.button`
+  width: 10.13rem;
+  height: 3.25rem;
+  border-radius: 8px;
+  border: none;
+  color: #fff;
+  background-color: #3e51cc;
+  outline: none;
+`;
