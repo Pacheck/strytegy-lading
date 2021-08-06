c7bd3a15adb588f8b93f6a4f2b7c4686082c1172
Pedro Pacheco <pacheckdev@gmail.com>
2021-08-06 19:31:08 -0300

    adding mock content

---
+++ src/mock/collaborationContent.js
@@ -0,0 +1,35 @@
+// import FirstImage from '../assets/Textura-Ciano.png';
+import FirstImage from '../assets/Textura-Rosa.png';
+import SecondImage from '../assets/Textura-Vermelha.png';
+import ThirdImage from '../assets/Textura-Laranja.png';
+
+import FirstContentImage from '../assets/Grafico-2.svg';
+import SecondContentImage from '../assets/Grafico-1.svg';
+import ThirdContentImage from '../assets/Grafico-3.svg';
+
+export const CollaborationContent = [
+  {
+    id: 1,
+    image: FirstImage,
+    internalImage: FirstContentImage,
+    headerText: 'Empower teams with digital walls of thinking',
+    bodyText:
+      'Strytegy is the brain of the organization. Evolve teams with journeys and share learning and knowledge with everyone. Hyper connect operations to strategy and lead intentional changes.',
+  },
+  {
+    id: 2,
+    image: SecondImage,
+    internalImage: SecondContentImage,
+    headerText: 'Travel and discover in practices journeys',
+    bodyText:
+      'More important than the end is the way. Build a journey with multimedia modeling features to draw and develop business models, solve the domain and complementary problems with world-class practices.',
+  },
+  {
+    id: 3,
+    image: ThirdImage,
+    internalImage: ThirdContentImage,
+    headerText: 'Hyper connect knowledge to see beyond',
+    bodyText:
+      'Think like a honeycomb or with a whole hive. From a single concept or an entire network, see your organisation through the lens of collaboration and critical reflection, not command and control.',
+  },
+];
---
+++ src/mock/coreContent.js
@@ -0,0 +1,31 @@
+import FirstImage from '../assets/Imagem-1.png';
+import SecondImage from '../assets/Imagem-2.png';
+import ThirdImage from '../assets/Imagem-3.png';
+import FourthImage from '../assets/Imagem-4.png';
+
+export const mockedContent = [
+  {
+    id: '1',
+    image: FirstImage,
+    title: 'Launch a new business',
+    text: 'Turn ideas into a clear action plan, validate, learn and grow.',
+  },
+  {
+    id: '2',
+    image: SecondImage,
+    title: 'Develop a digital product',
+    text: 'Map and prioritize features to build a product people love.',
+  },
+  {
+    id: '3',
+    image: ThirdImage,
+    title: 'Understand your customers',
+    text: 'Dive deep into their world to master its experience.',
+  },
+  {
+    id: '4',
+    image: FourthImage,
+    title: 'Lead and empower people',
+    text: 'Listen and co create a desirable (successful) future.',
+  },
+];
