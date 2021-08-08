34117b6d910dcda025d1f7f96cd7b3637cc5c226
Pedro Pacheco <pacheckdev@gmail.com>
2021-08-08 13:16:44 -0300

    adding mocked data

---
+++ src/mock/FAQContent.js
@@ -0,0 +1,19 @@
+export const FAQMock = [
+  {
+    header: 'Using our tools, Canvases & Methods',
+    text: 'Texto que responde a pergunta clicada pelo usuário. Segunda linha do parágrafo explicativo para esta pergunta da lista de perguntas feitas frequentemente.',
+  },
+  {
+    header: 'How to get started?',
+    text: 'Texto que responde a pergunta clicada pelo usuário. Segunda linha do parágrafo explicativo para esta pergunta da lista de perguntas feitas frequentemente.',
+  },
+  {
+    header: 'We got tips for you here!',
+    text: 'Texto que responde a pergunta clicada pelo usuário. Segunda linha do parágrafo explicativo para esta pergunta da lista de perguntas feitas frequentemente.',
+  },
+  {
+    header:
+      'How do I use the 9 building blocks on the canvas model to find my business model?',
+    text: 'Texto que responde a pergunta clicada pelo usuário. Segunda linha do parágrafo explicativo para esta pergunta da lista de perguntas feitas frequentemente.',
+  },
+];
